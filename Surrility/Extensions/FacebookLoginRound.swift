842ff25425d95599bb8b17007aa2c4fc32257318
Manish Agarwal <manish192delhi@hotmail.com>
2018-05-12 16:21:16 +0530

    Camera Layout changed and removed profile button

---
+++ Surrility/Extensions/FacebookLoginRound.swift
@@ -0,0 +1,61 @@
+//
+//  FacebookLoginRound.swift
+//  Surrility
+//
+//  Created by Manish Agarwal on 16/04/18.
+//  Copyright © 2018 SurrealMX. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FBSDKLoginKit
+
+@IBDesignable class FacebookLoginRound: FBSDKLoginButton
+{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        // Common logic goes here
+        refreshCorners(value: cornerRadius)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
